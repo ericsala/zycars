659bd7f9054729a08e352d523d766be0c38ab84d
dartmidder <dartmidder@dec2a252-8a97-a28e-c821-f0892bc11fcb>
2011-04-09 17:23:57 +0000

    Trabajando en la busqueda de caminos, completada a falta de probar

---
+++ branches/astar/run_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from engine import game
+
+def main():
+    '''
+    @brief Función main
+    '''
+    Game = game.Game()
+    Game.run()
+    
+if __name__ == "__main__":
+    main()
